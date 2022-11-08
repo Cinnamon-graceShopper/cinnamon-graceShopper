7a03f862338d545231886b82ca36451f090b8b41
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-08 17:51:17 -0500

    Dummy data validated for Coffee and User

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,12 @@
 //this is the access point for all things database related!
-
-const db = require('./db')
-
-const User = require('./models/User')
+const db = require('./db');
+const Coffee = require('./models/Coffee.js');
+const User = require('./models/User.js');
 
 //associations could go here!
 
 module.exports = {
   db,
-  models: {
-    User,
-  },
-}
+  User,
+  Coffee,
+};
