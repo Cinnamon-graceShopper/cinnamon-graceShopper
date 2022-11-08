06387ea677c05a5bf2aeb1e05c472faad76c2a2e
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-08 15:13:57 -0500

    removed Coffee id

--- seed.js
+++ seed.js
@@ -10,7 +10,6 @@ const coffees = [
     ingredients: ['Coffee'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/640px-A_small_cup_of_coffee.JPG',
-    id: 1,
   },
   {
     title: 'Latte',
@@ -19,7 +18,6 @@ const coffees = [
     ingredients: ['Espresso', 'Steamed Milk'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Latte_at_Doppio_Ristretto_Chiang_Mai_01.jpg/509px-Latte_at_Doppio_Ristretto_Chiang_Mai_01.jpg',
-    id: 2,
   },
   {
     title: 'Cappuccino',
@@ -28,7 +26,6 @@ const coffees = [
     ingredients: ['Espresso', 'Steamed Milk', 'Foam'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/e/ed/Wet_Cappuccino_with_heart_latte_art.jpg',
-    id: 3,
   },
   {
     title: 'Americano',
@@ -37,7 +34,6 @@ const coffees = [
     ingredients: ['Espresso', 'Hot Water'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Hokitika_Cheese_and_Deli%2C_Hokitika_%283526706594%29.jpg/1280px-Hokitika_Cheese_and_Deli%2C_Hokitika_%283526706594%29.jpg',
-    id: 4,
   },
   {
     title: 'Espresso',
@@ -46,7 +42,6 @@ const coffees = [
     ingredients: ['1oz Espresso'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/a/a5/Tazzina_di_caff%C3%A8_a_Ventimiglia.jpg',
-    id: 5,
   },
   {
     title: 'Doppio',
@@ -55,7 +50,6 @@ const coffees = [
     ingredients: ['2oz Espresso'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Doppio.jpg/2560px-Doppio.jpg',
-    id: 6,
   },
   {
     title: 'Cortado',
@@ -64,7 +58,6 @@ const coffees = [
     ingredients: ['1oz Espresso', '1oz Steamed Milk'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/1/16/Caf%C3%A9Cortado%28Tallat%29.jpg',
-    id: 7,
   },
   {
     title: 'Red Eye',
@@ -73,7 +66,6 @@ const coffees = [
     ingredients: ['Coffee', 'Espresso'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/f/f7/Linea_doubleespresso.jpg',
-    id: 8,
   },
   {
     title: 'Galão',
@@ -82,7 +74,6 @@ const coffees = [
     ingredients: ['Espresso', 'Foamed milk'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Gal%C3%A3o.jpg/1280px-Gal%C3%A3o.jpg',
-    id: 9,
   },
   {
     title: 'Lungo',
@@ -91,7 +82,6 @@ const coffees = [
     ingredients: ['Long pulled espresso'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/6/6a/Caff%C3%A8_lungo.JPG',
-    id: 10,
   },
   {
     title: 'Macchiato',
@@ -100,7 +90,6 @@ const coffees = [
     ingredients: ['Espresso', 'Foam'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/0/07/Caff%C3%A8_Espresso_Macchiato_Schiumato.jpg',
-    id: 11,
   },
   {
     title: 'Mocha',
@@ -109,7 +98,6 @@ const coffees = [
     ingredients: ['Espresso', 'Steamed Milk', 'Chocolate'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/f/f6/Mocaccino-Coffee.jpg',
-    id: 12,
   },
   {
     title: 'Ristretto',
@@ -118,7 +106,6 @@ const coffees = [
     ingredients: ['Short pulled espresso'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/1/12/Doppio_ristretto_Chiang_Mai.jpg',
-    id: 13,
   },
   {
     title: 'Flat White',
@@ -127,7 +114,6 @@ const coffees = [
     ingredients: ['Espresso', 'Steamed Milk'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/6/6b/Flat_white_coffee_with_pretty_feather_pattern.jpg',
-    id: 14,
   },
   {
     title: 'Affogato',
@@ -136,7 +122,6 @@ const coffees = [
     ingredients: ['Espresso', 'Ice cream'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/1/17/Vinoteca%2C_Smithfield%2C_London_%284485849609%29.jpg',
-    id: 15,
   },
   {
     title: 'Café au Lait',
@@ -144,7 +129,6 @@ const coffees = [
       'Café au lait is perfect for the coffee minimalist who wants a bit more flavor. Just add a splash of warm milk to your coffee and you’re all set!',
     ingredients: ['Coffee', 'Steamed Milk'],
     image: 'https://upload.wikimedia.org/wikipedia/commons/0/06/Latte_art.jpg',
-    id: 16,
   },
   {
     title: 'Irish',
@@ -153,7 +137,6 @@ const coffees = [
     ingredients: ['Coffee', 'Whiskey', 'Sugar', 'Cream'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/6/61/Irish_coffee_glass.jpg',
-    id: 17,
   },
   {
     title: 'Guayoyo',
@@ -162,7 +145,6 @@ const coffees = [
     ingredients: ['Coffee', 'Traditional', 'Hot Water'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/640px-A_small_cup_of_coffee.JPG',
-    id: 18,
   },
   {
     title: 'Cortadito',
@@ -171,7 +153,6 @@ const coffees = [
     ingredients: ['Coffee', 'Traditional', 'Sugar', 'Milk'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/9/9b/Cuban_coffee-_2013-04-05_14-30.jpg',
-    id: 19,
   },
   {
     title: 'Aguapanela Coffee',
@@ -180,7 +161,6 @@ const coffees = [
     ingredients: ['Coffee', 'Sweet', 'Panela', 'Traditional'],
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Agua_Panela_con_Queso_Fresco_at_La_Puerta_Falsa_%285617496209%29.jpg/640px-Agua_Panela_con_Queso_Fresco_at_La_Puerta_Falsa_%285617496209%29.jpg',
-    id: 20,
   },
 ];
 
