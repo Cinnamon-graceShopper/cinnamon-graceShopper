ade0aa2080b0a619380cb1b0600db9e6f091d601
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-10 15:18:27 -0500

    Added Admind Backend Routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,13 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.use("/users", require("./users"));
-router.use("/coffees", require("./coffees"));
+router.use('/users', require('./users'));
+router.use('/coffees', require('./coffees'));
+router.use('/admin', require('./admin'));
 
 router.use((req, res, next) => {
-	const error = new Error("Not Found");
-	error.status = 404;
-	next(error);
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
 module.exports = router;
