a2239a932996f0aa7af2c2888ce8db365c3431f4
Tasdeed <thossai4@binghamton.edu>
2022-11-15 16:08:56 -0500

    got some nice buttons/ nav bar stuff

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
 import { connect } from "react-redux";
 import { authenticate } from "../store";
@@ -6,30 +7,47 @@ import { authenticate } from "../store";
  * COMPONENT
  */
 const AuthForm = (props) => {
-	const { name, displayName, handleSubmit, error } = props;
+  const { name, displayName, handleSubmit, error } = props;
 
-	return (
-		<div>
-			<form onSubmit={handleSubmit} name={name}>
-				<div>
-					<label htmlFor='username'>
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          {/* <label htmlFor='username'>
 						<small>Email</small>
 					</label>
-					<input name='username' type='text' />
-				</div>
-				<div>
-					<label htmlFor='password'>
-						<small>Password</small>
-					</label>
-					<input name='password' type='password' />
-				</div>
-				<div>
-					<button type='submit'>{displayName}</button>
-				</div>
-				{error && error.response && <div> {error.response.data} </div>}
-			</form>
-		</div>
-	);
+					<input name='username' type='text' /> */}
+          <TextField
+            id="username"
+            name="username"
+            type="text"
+            label="Email"
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" /> */}
+          <TextField
+            id="password"
+            name="password"
+            type="password"
+            label="Password"
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* <button type="submit">{displayName}</button> */}
+          <Button variant="contained" color="success" type="submit">
+            {displayName}
+          </Button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
+  );
 };
 
 /**
@@ -40,23 +58,23 @@ const AuthForm = (props) => {
  *   can stay DRY with interfaces that are very similar to each other!
  */
 const mapLogin = (state) => {
-	return {
-		name: "login",
-		displayName: "Login",
-		error: state.auth.error,
-	};
+  return {
+    name: "login",
+    displayName: "Login",
+    error: state.auth.error,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-	return {
-		handleSubmit(evt) {
-			evt.preventDefault();
-			const formName = evt.target.name;
-			const username = evt.target.username.value;
-			const password = evt.target.password.value;
-			dispatch(authenticate(username, password, formName));
-		},
-	};
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const username = evt.target.username.value;
+      const password = evt.target.password.value;
+      dispatch(authenticate(username, password, formName));
+    },
+  };
 };
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm);
--- client/components/SignUpForm.js
+++ client/components/SignUpForm.js
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
 import { connect } from "react-redux";
 import { signup } from "../store/user";
@@ -7,42 +8,73 @@ import { signup } from "../store/user";
  * COMPONENT
  */
 const SignUpForm = (props) => {
-	const { name, displayName, handleSubmit, error } = props;
+  const { name, displayName, handleSubmit, error } = props;
 
-	return (
-		<div>
-			<form onSubmit={handleSubmit} name={name}>
-				<div>
-					<label htmlFor='username'>
-						<small>Email</small>
-					</label>
-					<input name='username' type='text' />
-				</div>
-				<div>
-					<label htmlFor='password'>
-						<small>Password</small>
-					</label>
-					<input name='password' type='password' />
-				</div>
-				<div>
-					<label htmlFor='firstName'>
-						<small>First Name</small>
-					</label>
-					<input name='firstName' type='firstName' />
-				</div>
-				<div>
-					<label htmlFor='lastName'>
-						<small>Last Name</small>
-					</label>
-					<input name='lastName' type='lastName' />
-				</div>
-				<div>
-					<button type='submit'>{displayName}</button>
-				</div>
-				{error && error.response && <div> {error.response.data} </div>}
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          {/* <label htmlFor="username">
+            <small>Email</small>
+          </label>
+          <input name="username" type="text" /> */}
+          <TextField
+            required
+            name="username"
+            type="text"
+            label="Email"
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" /> */}
+          <TextField
+            required
+            name="password"
+            type="password"
+            label="Password"
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="firstName" /> */}
+          <TextField
+            required
+            name="firstName"
+            type="firstName"
+            label="First Name"
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="lastName" /> */}
+          <TextField
+            required
+            name="lastName"
+            type="lastName"
+            label="Last Name"
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* <button type="submit">{displayName}</button> */}
+          <Button variant="contained" color="success" type="submit">
+            {displayName}
+          </Button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
+  );
 };
 
 /**
@@ -54,26 +86,26 @@ const SignUpForm = (props) => {
  */
 
 const mapSignup = (state) => {
-	return {
-		name: "signup",
-		displayName: "Sign Up",
-		error: state.auth.error,
-	};
+  return {
+    name: "signup",
+    displayName: "Sign Up",
+    error: state.auth.error,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-	return {
-		handleSubmit(evt) {
-			evt.preventDefault();
-			const formName = evt.target.name;
-			const username = evt.target.username.value;
-			const password = evt.target.password.value;
-			const firstName = evt.target.firstName.value;
-			const lastName = evt.target.lastName.value;
-			dispatch(signup(username, password, firstName, lastName, formName));
-			//dispatch(authenticate(username, password, formName));
-		},
-	};
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const username = evt.target.username.value;
+      const password = evt.target.password.value;
+      const firstName = evt.target.firstName.value;
+      const lastName = evt.target.lastName.value;
+      dispatch(signup(username, password, firstName, lastName, formName));
+      //dispatch(authenticate(username, password, formName));
+    },
+  };
 };
 
 export const Signup = connect(mapSignup, mapDispatch)(SignUpForm);
