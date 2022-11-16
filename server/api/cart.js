ee91fc9f4b99c8c9e9b722c843880228a357a3e0
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-16 11:34:07 -0500

    creating persistant cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -32,12 +32,44 @@ router.get('/:id', requireToken, async (req, res, next) => {
 });
 
 // We will use this route to eventually create the OrderCoffee through table instances
-router.post('/', async (req, res, next) => {
+router.post('/:id', requireToken, async (req, res, next) => {
   try {
-    const post = await Order.create();
+    const reduxCart = req.body;
+    const loggedUserId = req.user.id;
+    const userCart = await Order.findOne({
+      where: {
+        userId: loggedUserId,
+        completed: false,
+      },
+    });
+    const userCartId = userCart.id;
+    reduxCart.forEach(async (product) => {
+      const dataToSend = {
+        quantity: product.cartQuantity,
+        price: product.price,
+        orderId: userCartId,
+        coffeeId: product.id,
+      };
+      await OrderCoffee.create(dataToSend);
+    });
+    res
+      .status(200)
+      .send(`Completed creating cart items for cart ${userCartId}`);
   } catch (error) {
     next(error);
   }
 });
 
 module.exports = router;
+
+// We would need the USER ID to find ORDER ID where Complete: false
+
+// [{
+//   id: 3,
+//   price: "1.99",
+//   cartQuantity: 2
+// }, {
+//   id: 4,
+//   price: "1.99",
+//   cartQuantity: 5
+// }]
