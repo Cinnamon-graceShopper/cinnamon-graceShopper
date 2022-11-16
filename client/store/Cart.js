fb2261725e545c9e30f5e18198fd5afd539eafc6
Jerral Graham <jerralgraham@yahoo.com>
2022-11-16 15:57:28 -0500

    decrement working

--- client/store/Cart.js
+++ client/store/Cart.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const ADD_CART = 'ADD_CART';
 const CREATE_CART = 'CREATE_CART';
 const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
+const DECREMENT = 'DECREMENT'
 
 const _addCart = (coffee) => ({
 	type: ADD_CART,
@@ -18,6 +19,12 @@ export const _removeProduct = (coffee) => ({
 	type: REMOVE_PRODUCT,
 	coffee,
 });
+
+export const decrement = (coffee) => ({
+	type: DECREMENT,
+	coffee,
+});
+
 
 export const addCart = (id) => async (dispatch) => {
 	try {
@@ -38,25 +45,43 @@ export const createCart = (cart) => async (dispatch) => {
 };
 
 const initialState = [];
-let coffeeIndex, tempProduct;
+let coffeeIndex, tempProduct, reduceIndex;
 
 export default function addCartReducer(state = initialState, action) {
 	switch (action.type) {
 		case ADD_CART:
-			tempProduct = { ...action.coffee, cartQuantity: 1 };
-			coffeeIndex = state.findIndex((item) => item.id === action.coffee.id);
-			if (coffeeIndex >= 0) {
-				state[coffeeIndex].cartQuantity += 1;
-				localStorage.setItem('cart', JSON.stringify([...state]));
-			} else {
-				localStorage.setItem('cart', JSON.stringify([...state, tempProduct]));
-				return [...state, tempProduct];
-			}
+        tempProduct = { ...action.coffee, cartQuantity: 1 };
+        coffeeIndex = state.findIndex((item) => item.id === action.coffee.id);
+        if (coffeeIndex >= 0) {
+          state[coffeeIndex].cartQuantity += 1;
+          localStorage.setItem('cart', JSON.stringify([...state]));
+        } else {
+          localStorage.setItem('cart', JSON.stringify([...state, tempProduct]));
+          return [...state, tempProduct];
+        }
 			return [...state];
+      case DECREMENT:
+        reduceIndex = state.findIndex(item=> item.id == action.coffee )
+        console.log( state)
+        if(state[reduceIndex].cartQuantity > 1){
+
+          state[reduceIndex].cartQuantity -= 1
+           localStorage.setItem('cart', JSON.stringify([...state]));
+        }
+        return [...state];
 		default:
 			return state;
 	}
 }
+
+
+
+
+
+
+
+
+
 
 // case CREATE_CART:
 // 	return [...state, action.coffee];
